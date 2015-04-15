8f9b30b4630ccc444f0db44a7aa41496f8b64f19
Kyle Giard-Chase & Liz Beacham <student@epicodus.com>
2015-04-15 11:02:05 -0700

    Add make account form, add jQuery methods to display output in #balance div

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,49 @@ var BankAccount = {
     }
 };
 
+var Person = {
+  fullName: "",
+  accountBalance: BankAccount.balance
+};
+
 
 
 $(document).ready(function() {
-    $("form#bank-account").submit(function(event) {
+    $("form#make-account").submit(function(event) {
         event.preventDefault();
 
-        var inputtedDeposit = parseInt($("input#deposit").val());
-        var inputtedWithdrawal = parseInt($("input#withdrawal").val());
+        var inputtedName = $("input#name").val();
+        var inputtedFirstDeposit = parseInt($("input#first-deposit").val());
+
+        var newBankAccount = Object.create(BankAccount);
+        //newBankAccount.balance = inputtedFirstDeposit;
 
 
+        var newPerson = Object.create(Person);
+        newPerson.fullName = inputtedName;
+        newPerson.accountBalance = inputtedFirstDeposit;
 
+
+        $('#balance').show();
+        $('.customer').text(newPerson.fullName);
+        $('.bank-balance').text(newPerson.accountBalance);
 
         //$("ul#deposit")
-    })
-})
+    });
+
+    // $("form#deposit-withdraw").submit(function(event) {
+    //     event.preventDefault();
+    //
+    //     var inputtedDeposit = parseInt($("input#deposit").val());
+    //     var inputtedWithdraw = parseInt($("input#withdraw").val());
+    //
+    //     var newBankAccount = Object.create(BankAccount);
+    //     var newPerson = Object.create(Person);
+    //     newBankAccount.balance = newPerson.accountBalance;
+    //
+    //     $('.balance').show();
+    //     $('.bank-balance').text(newBankAccount.balance);
+    //
+    //
+    // });
+});
