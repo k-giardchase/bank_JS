41993ad6b3167f5ec8540cbf00489d52f6695354
Kyle Giard-Chase & Liz Beacham <student@epicodus.com>
2015-04-15 11:53:43 -0700

    Fix objects so that inheritence works

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,6 @@ var BankAccount = {
     }
 };
 
-var Person = {
-  fullName: "",
-  accountBalance: BankAccount.balance
-};
-
-
-
 $(document).ready(function() {
     $("form#make-account").submit(function(event) {
         event.preventDefault();
@@ -23,34 +16,39 @@ $(document).ready(function() {
         var inputtedFirstDeposit = parseInt($("input#first-deposit").val());
 
         var newBankAccount = Object.create(BankAccount);
-        //newBankAccount.balance = inputtedFirstDeposit;
-
-
-        var newPerson = Object.create(Person);
-        newPerson.fullName = inputtedName;
-        newPerson.accountBalance = inputtedFirstDeposit;
+        newBankAccount.customerName = inputtedName;
+        newBankAccount.balance = inputtedFirstDeposit;
 
 
         $('#balance').show();
-        $('.customer').text(newPerson.fullName);
-        $('.bank-balance').text(newPerson.accountBalance);
+        $('.customer').text(newBankAccount.customerName);
+        $('.bank-balance').text(newBankAccount.balance);
 
-        //$("ul#deposit")
+        $("form#deposit-withdraw").submit(function(event) {
+            event.preventDefault();
+
+            var inputtedDeposit = parseInt($("input#deposit").val());
+            var inputtedWithdraw = parseInt($("input#withdraw").val());
+
+
+            // var newBankAccount = Object.create(BankAccount);
+            // var newPerson = Object.create(Person);
+            if(isNaN(inputtedWithdraw)) {
+              return newBankAccount.balance;
+            } else {
+              newBankAccount.withdraw(inputtedWithdraw);
+              // newPerson.accountBalance = newBankAccount.balance;
+            }
+
+            // if(!(isNaN(inputtedDeposit !== NaN) {
+            //   newBankAccount.deposit(inputtedDeposit);
+            //   newPerson.accountBalance = newBankAccount.balance;
+            // }
+
+            $('#balance').show();
+            $('.bank-balance').text(newBankAccount.balance);
+        });
     });
 
-    // $("form#deposit-withdraw").submit(function(event) {
-    //     event.preventDefault();
-    //
-    //     var inputtedDeposit = parseInt($("input#deposit").val());
-    //     var inputtedWithdraw = parseInt($("input#withdraw").val());
-    //
-    //     var newBankAccount = Object.create(BankAccount);
-    //     var newPerson = Object.create(Person);
-    //     newBankAccount.balance = newPerson.accountBalance;
-    //
-    //     $('.balance').show();
-    //     $('.bank-balance').text(newBankAccount.balance);
-    //
-    //
-    // });
+
 });
