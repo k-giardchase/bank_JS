980f88905544c5d9f78156357da57d13f2fe3871
Kyle Giard-Chase & Liz Beacham <student@epicodus.com>
2015-04-15 10:08:49 -0700

    Pass first test for BankAccount withdraw

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var BankAccount = {
+    balance: 0,
+    withdraw: function(amount) {
+        this.balance = this.balance - amount;
+    }
+};
+
+
+
+$(document).ready(function() {
+    $("form#bank-account").submit(function(event) {
+        event.preventDefault();
+
+        var inputtedDeposit = parseInt($("input#deposit").val());
+        var inputtedWithdrawal = parseInt($("input#withdrawal").val());
+
+
+
+
+        //$("ul#deposit")
+    })
+})
---
+++ specs/specs.js
@@ -0,0 +1,9 @@
+describe("BankAccount", function() {
+    describe("withdraw", function() {
+        it("withdraws the specified amount and returns the new balance", function() {
+            var testBankAccount = Object.create(BankAccount);
+            testBankAccount.withdraw(50);
+            expect(testBankAccount.balance).to.equal(-50);
+        });
+    });
+});
